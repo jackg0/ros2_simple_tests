907792dfcc44f63b7ecc15e54de622ca278fd5fc
Jack Geissinger <jgeissinger@carnegierobotics.com>
2024-03-26 15:52:10 -0400

    server: Add spin period for spinning.

--- simple_server.cc
+++ simple_server.cc
@@ -1,3 +1,5 @@
+#include <getopt.h>
+
 #include <nav_msgs/msg/odometry.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <std_srvs/srv/trigger.hpp>
@@ -6,11 +8,65 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rcutils/logging.h>
 
+struct Options
+{
+    size_t spinPeriodMilliseconds{30};
+};
+
+void showHelp(const std::string &name)
+{
+    std::cout << "usage: " << name << " OPTIONS\n"
+        "OPTIONS:\n"
+        " -t | --spin-period-milliseconds     spin period (ms); valid bounds: [1, 1000].\n"
+        " -h | --help                         show this help.\n";
+}
+
+Options parseOpts(int argc, char **argv)
+{
+    static constexpr auto shortOpts = "t:h";
+    static constexpr struct option longOpts[] = {
+        { "spin-period-milliseconds", required_argument, nullptr, 't' },
+        { "help", no_argument, nullptr, 'h' },
+        { nullptr, 0, nullptr, 0 }
+    };
+
+    Options opts{ };
+
+    for (int c = 0; (c = getopt_long(argc, argv, shortOpts, longOpts, nullptr)) >= 0; )
+    {
+        switch (c)
+        {
+            case 't':
+                opts.spinPeriodMilliseconds = std::clamp<size_t>(std::stod(optarg), 1, 1000);
+                break;
+            case 'h':
+                showHelp(argv[0]);
+                std::exit(0);
+            case '?':
+                std::exit(1);
+            default:
+                break;
+        }
+    }
+
+    if (optind < argc)
+    {
+        std::cerr << argv[0] << ": trailing args..\n";
+        std::exit(1);
+    }
+
+    return opts;
+}
+
 int main(int argc, char **argv)
 {
     using namespace std::chrono_literals;
 
     rclcpp::init(argc, argv, rclcpp::InitOptions());
+
+    auto opts = parseOpts(argc, argv);
+
+    RCLCPP_INFO(rclcpp::get_logger("simple_server"), "spin period (ms): %zu", opts.spinPeriodMilliseconds);
 
     auto node = std::make_shared<rclcpp::Node>("simple_server");
 
@@ -54,13 +110,19 @@ int main(int argc, char **argv)
 
     executor.add_node(node);
 
+    auto spinPeriod = std::chrono::milliseconds(opts.spinPeriodMilliseconds);
+
     while (rclcpp::ok())
     {
+        const auto rateDeadline = std::chrono::steady_clock::now() + spinPeriod;
+
         RCLCPP_INFO(rclcpp::get_logger("simple_server"), "before spin");
 
-        executor.spin_all(30ms);
+        executor.spin_all(spinPeriod);
 
         RCLCPP_INFO(rclcpp::get_logger("simple_server"), "after spin");
+
+        std::this_thread::sleep_until(rateDeadline);
     }
 
     return 0;
